2c24e3918e0ee993f1ddf5ce9e91417fa87674cf
TyagiShishira <ayushityagi601@gmail.com>
2021-09-28 19:58:12 +0530

    Initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SignupFramework' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'LoginFramework', :path => '/Users/tyagi/Desktop/LoginFramework' 
+
+  # Pods for SignupFramework
+
+end
---
+++ SignupFramework/SignupFramework.h
@@ -0,0 +1,18 @@
+//
+//  SignupFramework.h
+//  SignupFramework
+//
+//  Created by Tyagi on 28/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SignupFramework.
+FOUNDATION_EXPORT double SignupFrameworkVersionNumber;
+
+//! Project version string for SignupFramework.
+FOUNDATION_EXPORT const unsigned char SignupFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignupFramework/PublicHeader.h>
+
+
